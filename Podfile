6b5d85c26ec0d59cc09b2ed5faab22ced0f8f2c1
xiaoP <anddygong@gmail.com>
2017-04-11 16:59:02 +0800

    init project

---
+++ Podfile
@@ -0,0 +1,17 @@
+# Uncomment the next line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'RNTest' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for RNTest
+  pod 'React', :path => '../js/node_modules/react-native', :subspecs => [
+    'Core',
+    'RCTImage',
+    'RCTNetwork',
+    'RCTText',
+    'RCTWebSocket',
+  ]
+
+end
---
+++ RNTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  RNTest
+//
+//  Created by xiaoP on 2017/4/11.
+//  Copyright © 2017年 Chicv. All rights reserved.
+//
+
+import UIKit
+import React
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
